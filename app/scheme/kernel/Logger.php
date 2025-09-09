7acf14f8d48693b67977d99be81ee1c59fbd3868
RON <h4ckw1z@gmail.com>
2025-09-09 21:25:55 +0800

    updated to version v4.2.5

--- app/scheme/kernel/Logger.php
+++ app/scheme/kernel/Logger.php
@@ -29,19 +29,50 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
  *
  * @package LavaLust
  * @author Ronald M. Marasigan <ronald.marasigan@yahoo.com>
- * @since Version 1
+ * @since Version 4
  * @link https://github.com/ronmarasigan/LavaLust
  * @license https://opensource.org/licenses/MIT MIT License
  */
 
-/*
-| -------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------
-| Here is where you can register web routes for your application.
-|
-|
-*/
+/**
+* ------------------------------------------------------
+*  Class Logger
+* ------------------------------------------------------
+ */
+class Logger {
 
-$router->get('/', 'Welcome::index');
-$router->get('/test/about', 'test/TestController::about');
+    /**
+     * log error etc
+     *
+     * @param string $type | debug, error
+     * @param string $header
+     * @param string $message
+     * @param string $filename
+     * @param string $linenum
+     * @return void
+     */
+    public function log($type = '', $header = '', $message = '', $filename = '', $linenum = '')
+    {
+        $logfile = config_item('log_dir').'log.txt';
+        if (! file_exists($logfile)) { 
+            mkdir(config_item('log_dir'), 0777, true);
+            $fh = fopen($logfile, 'w');
+            fclose($fh);
+        } 
+
+        $date = date("d/m/Y G:i:s");
+
+        if($type == 'debug' && (config_item('log_threshold') == 2 || config_item('log_threshold') == 3))
+        {
+            $err = "Date: ".$date."\n"."Debug Message: ".$header;
+            $err .= "\n------------------------------------------------------------------\n\n";
+            error_log($err, 3, $logfile);
+        } else if($type == 'error' && (config_item('log_threshold') == 1 || config_item('log_threshold') == 3))
+        {
+            $message = is_array($message)? implode("\n", $message): $message;
+            $err = "Date: ".$date."\n"."Exception Class: ".$header."\n"."Error Message: ".$message."\n"."Filename: ".$filename."\n"."Line Number: ".$linenum;
+            $err .= "\n------------------------------------------------------------------\n\n";
+            error_log($err, 3, $logfile);
+        }  
+    }
+ }
