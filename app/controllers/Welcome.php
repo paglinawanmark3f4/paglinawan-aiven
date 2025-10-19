8d6a8b1d897f01a8a4bff4349a76e7c055762ad5
RON <h4ckw1z@gmail.com>
2025-10-19 12:11:05 +0800

    fixed bugs

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -3,7 +3,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class Welcome extends Controller {
 	public function index() {
-		$this->call->library('Ember');
 		$this->call->view('welcome_page');
 	}
 }
